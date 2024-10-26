6019e2d1bf5cb5f74e56380980a5c73535dfe303
benioriginalul <111044589+benioriginal@users.noreply.github.com>
2024-10-26 17:15:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const desktopVer = document.getElementById('desktopVer')
+const mobileVer = document.getElementById('mobileVer')
+
+
+function throttle(func, delay) {
+    let lastCall = 0;
+    return function(...args) {
+      const now = new Date().getTime();
+      if (now - lastCall < delay) {
+        return;
+      }
+      lastCall = now;
+      return func(...args);
+    };
+  }
+  
+  if (window.innerWidth <= 768) {
+    const mobileVer = document.getElementById('mobileVer');
+    const desktopVer = document.getElementById('desktopVer');
+    mobileVer.style.display = 'block';
+    desktopVer.remove();
+    let menuDown = false
+    const extendedMenu = document.getElementById('extendedMenu');
+    mobileVer.style.transform = `translateY(-${extendedMenu.offsetHeight + 5}px)`;
+    console.log(extendedMenu.offsetHeight);
+    
+    const menuButton = document.getElementById('menuButton');
+    menuButton.addEventListener("click", () => {
+      const offsetHeight = extendedMenu.offsetHeight + 5;
+      if (!menuDown){
+
+          mobileVer.animate([
+            { transform: `translateY(-${offsetHeight}px)` },
+            { transform: `translateY(0)` }
+          ], {
+            duration: 300,
+            fill: 'forwards'
+          });
+          menuDown = !menuDown
+    }
+    else{
+        mobileVer.animate([
+            { transform: `translateY(0)` },
+            { transform: `translateY(-${offsetHeight}px)` }
+          ], {
+            duration: 300,
+            fill: 'forwards'
+          });
+          menuDown = !menuDown
+    }
+
+  });
+}
+  
+else{
+    mobileVer.remove()
+}
