02771cdcf5ce87499346f6e65468c6e689a84a28
benioriginalul <111044589+benioriginal@users.noreply.github.com>
2024-10-26 18:01:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,6 +23,42 @@ function throttle(func, delay) {
     const extendedMenu = document.getElementById('extendedMenu');
     mobileVer.style.transform = `translateY(-${extendedMenu.offsetHeight + 5}px)`;
     console.log(extendedMenu.offsetHeight);
+    window.onscroll = function(e) {
+      // print "false" if direction is down and "true" if up
+      if (this.oldScroll > this.scrollY){
+        menuDown = true
+        console.log("true")
+      }
+      else{
+        menuDown = false
+        console.log('false')
+      }
+      this.oldScroll = this.scrollY;
+      const offsetHeight = extendedMenu.offsetHeight + 5;
+      if (!menuDown){
+          extendedMenu.style.display = 'none'
+          mobileVer.animate([
+            { transform: `translateY(-${offsetHeight}px)` },
+            { transform: `translateY(0)` }
+          ], {
+            duration: 0,
+            fill: 'forwards'
+          });
+          menuDown = !menuDown
+    }
+    else{
+        extendedMenu.style.display = 'flex'
+
+        mobileVer.animate([
+            { transform: `translateY(0)` },
+            { transform: `translateY(-${offsetHeight}px)` }
+          ], {
+            duration: 0,
+            fill: 'forwards'
+          });
+          menuDown = !menuDown
+    }
+    }
     
     const menuButton = document.getElementById('menuButton');
     menuButton.addEventListener("click", () => {
@@ -50,6 +86,7 @@ function throttle(func, delay) {
     }
 
   });
+  
 }
   
 else{
